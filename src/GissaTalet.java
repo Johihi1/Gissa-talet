23b8e463ce6ce220bb29b0883650f3897814ca7e
Johihi1 <54669280+Johihi1@users.noreply.github.com>
2020-11-17 10:51:07 +0100

    Update GissaTalet.java

--- src/GissaTalet.java
+++ src/GissaTalet.java
@@ -3,11 +3,22 @@ import java.util.Random;
 public class GissaTalet {
     public static void main(String[] args){
         Random rand = new Random();
-        int Tal = rand.nextInt(100);
-        int Försök = 0;
+        int tal = rand.nextInt(100);
+        int försök = 0;
         Scanner input = new Scanner(System.in);
         int guess;
 
         System.out.println("Gissa ett tal mellan 1 och 100.");
+        guess = input.nextInt();
+
+        if (guess == tal){
+
+        }
+        else if(guess < tal){
+            System.out.println("För lågt.");
+        }
+        else if(guess > tal){
+            System.out.println("För högt.");
+        }
     }
 }
