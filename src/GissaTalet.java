ef2f202ec9871b1a4462afba9d523da5e1232c07
Johihi1 <54669280+Johihi1@users.noreply.github.com>
2020-11-17 10:47:03 +0100

    Update GissaTalet.java

--- src/GissaTalet.java
+++ src/GissaTalet.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+import java.util.Random;
 public class GissaTalet {
     public static void main(String[] args){
-        
+        Random rand = new Random();
+        int Tal = rand.nextInt(100);
+        int Försök = 0;
+        Scanner input = new Scanner(System.in);
+        int guess;
+
+        System.out.println("Gissa ett tal mellan 1 och 100.");
     }
 }
